9e95f81f5c2f032b8d685e7ffce8ac9e1b0b9dcc
Chhatarapati Chandril <chandril6a@gmail.com>
2025-12-31 17:55:35 +0530

    improve flood fill algorithm logic

--- src/18Graphs/FloodFill_Algo.java
+++ src/18Graphs/FloodFill_Algo.java
@@ -1,26 +1,32 @@
 public class FloodFill_Algo {
 
     // O(m * n)
-    public void helper(int[][] image, int sr, int sc, int color, boolean[][] vis, int orginalCol) {
-        // base case
-        if (sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length 
-            || vis[sr][sc] || image[sr][sc] != orginalCol) {
+    public void helper(int[][] image, int sr, int sc, int color, boolean[][] vis, int originalCol) {
+        if(sc < 0 || sr < 0 || sr>= image.length || sc >= image[0].length) {
             return;
         }
+        if(vis[sr][sc] || image[sr][sc] != originalCol) {
+            return;
+        }
+
+        vis[sr][sc] = true;
+
         image[sr][sc] = color;
+        
         // left
-        helper(image, sr, sc - 1, color, vis, orginalCol);
+        helper(image, sr, sc - 1, color, vis, originalCol);
         // right
-        helper(image, sr, sc + 1, color, vis, orginalCol);
-        // up
-        helper(image, sr - 1, sc, color, vis, orginalCol);
-        // down
-        helper(image, sr + 1, sc, color, vis, orginalCol);
+        helper(image, sr, sc + 1, color, vis, originalCol);
+        // top
+        helper(image, sr - 1, sc, color, vis, originalCol);
+        // bottom
+        helper(image, sr + 1, sc, color, vis, originalCol);
     }
-    
+
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         boolean[][] vis = new boolean[image.length][image[0].length];
-        helper(image, sr, sc, color, vis, color);
+        int originalCol = image[sr][sc];
+        helper(image, sr, sc, color, vis, originalCol);
         return image;
     }
 
